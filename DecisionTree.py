53971a6112422fb02fd5c4170086fa2502b1d506
sk929 <kumarsaurav929@gmail.com>
2020-05-25 23:33:08 +0530

    Update DecisionTree.py

--- DecisionTree.py
+++ DecisionTree.py
@@ -50,3 +50,17 @@ final_model = DecisionTreeRegressor(max_leaf_nodes = best_tree_size , random_sta
 
 # fit the final model and uncomment the next two lines
 final_model.fit(X, y)
+
+
+
+
+'''Random
+Forest'''
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+
+forest_model = RandomForestRegressor(random_state=1)
+forest_model.fit(train_X, train_y)
+melb_preds = forest_model.predict(val_X)
+print(mean_absolute_error(val_y, melb_preds))
